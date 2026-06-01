5c3b09d1bbc9747658643a5a21c71ff8ee1694fb
lmatsan <oetam.aicul@gmail.com>
2026-06-01 17:17:38 +0200

    Update MLflow run name for training pipeline to reflect feature engineering focus

--- src/model_trainer.py
+++ src/model_trainer.py
@@ -356,7 +356,7 @@ def run_training_pipeline() -> dict:
 
     mlflow.end_run() # Limpieza inicial
 
-    with mlflow.start_run(run_name="Hyperparameter Tuning (GridSearch)") as parent_run:
+    with mlflow.start_run(run_name="Feature Engineering: Cancelation ratio") as parent_run:
         parent_id = parent_run.info.run_id
 
         training_output = train_all_models(parent_run_id=parent_id)
